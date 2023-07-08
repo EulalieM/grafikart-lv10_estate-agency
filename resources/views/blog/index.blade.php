2a04a5ee15a46c1848b459be2b2bd74036a08932
Eulalie Moreau <eulalie.m16000@gmail.com>
2023-07-08 16:02:49 +0200

    discovery views with blade

---
+++ resources/views/blog/index.blade.php
@@ -0,0 +1,27 @@
+@extends('base')
+
+@section('title', 'Mon blog')
+
+@section('content')
+
+    <h1>Mon blog</h1>
+
+    <!-- @dump($posts) -->
+
+    @foreach ($posts as $post)
+        <article class="my-5">
+            <h2>{{ $post->title }}</h2>
+            <p>
+                {{ $post->content }}
+            </p>
+            <p>
+                <a class="btn btn-primary" href="{{ route('blog.show', ['slug' => $post->slug, 'id' => $post->id]) }}">
+                    Lire la suite
+                </a>
+            </p>
+        </article>
+    @endforeach
+
+    {{ $posts->links() }}
+
+@endsection
