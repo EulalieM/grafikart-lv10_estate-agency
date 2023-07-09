aa199f6f0d7278545f086cfb32aecf6814192b04
Eulalie Moreau <eulalie.m16000@gmail.com>
2023-07-09 10:49:13 +0200

    discovery data validation

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -6,12 +6,30 @@ use App\Models\Post;
 use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 use Illuminate\View\View;
 
 class BlogController extends Controller
 {
     public function index(): View
     {
+        $validator = Validator::make([
+            'title' => 'Mimimum 8 caractères',
+            'content' => 'Mon contenu'
+        ], [
+            'title' => 'required|min:8'
+            // 'title' => [
+            //     'required',
+            //     'min:8'
+            // ]
+            // 'title' => [Rule::unique('posts')->ignore(2)]
+        ]);
+
+        // dd($validator->fails()); // bool
+        // dd($validator->errors()); // messages d'erreur
+        dd($validator->validated()); // retourne seulement les éléments validés
+
         return view('blog.index', [
             'posts' => Post::paginate(2)
         ]);
