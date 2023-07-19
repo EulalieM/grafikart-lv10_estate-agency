258cac8e80c6196c48ff21fb02b55e623e9f89e0
Eulalie Moreau <eulalie.m16000@gmail.com>
2023-07-19 21:03:08 +0200

    discovery relations, create categories

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -17,6 +18,12 @@ class BlogController extends Controller
 {
     public function index(): View
     {
+        Category::create([
+            'name' => 'Catégorie 1'
+        ]);
+        Category::create([
+            'name' => 'Catégorie 2'
+        ]);
         return view('blog.index', [
             'posts' => Post::paginate(2)
         ]);
