ea642903aae3360b199f7e5723c24b8550aba421
Eulalie Moreau <eulalie.m16000@gmail.com>
2023-07-19 22:28:29 +0200

    delete relation

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -19,8 +19,7 @@ class BlogController extends Controller
     public function index(): View
     {
         $post = Post::find(2);
-        $tags = $post->tags()->where('name', 'Tag 1')->get();
-        dd($tags);
+        $tags = $post->tags()->detach(2);
 
         return view('blog.index', [
             'posts' => Post::paginate(2)
