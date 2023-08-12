79b707963ba082797ff1cc4df4df13c4ad3aa532
Eulalie Moreau <eulalie.m16000@gmail.com>
2023-08-12 13:18:18 +0200

    admin crud and index admin view

---
+++ app/Http/Controllers/Admin/PropertyController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Property;
+
+class PropertyController extends Controller
+{
+    public function index()
+    {
+        return view('admin.properties.index', [
+            'properties' => Property::orderBy('created_at', 'DESC')->paginate(25)
+        ]);
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function edit(string $id)
+    {
+        //
+    }
+
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    public function destroy(string $id)
+    {
+        //
+    }
+}
