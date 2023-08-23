93add1bc4b2e4d8357d56f5b89b598fd97914b04
Eulalie Moreau <eulalie.m16000@gmail.com>
2023-08-23 21:38:13 +0200

    nav admin

--- resources/views/admin/base.blade.php
+++ resources/views/admin/base.blade.php
@@ -10,6 +10,28 @@
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Agence</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            @php
+                $route = request()->route()->getName()
+            @endphp
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a href="{{ route('admin.property.index') }}" @class(['nav-link', 'active' => str_contains($route, 'property.')])>Gérer les biens</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('admin.option.index') }}" @class(['nav-link', 'active' => str_contains($route, 'option.')])>Gérer les options</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+      </nav>
+
     <div class="container my-5">
         @if (session('success'))
             <div class="alert alert-success">
